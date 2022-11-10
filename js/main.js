2ead13bcf6fca3949fa6e94e1a7d83e0afaf5f89
Sektories7 <Sektories7@gmail.com>
2022-11-10 03:20:32 +0200

    submit-btn update

--- js/main.js
+++ js/main.js
@@ -3,31 +3,29 @@ let rating_btn = document.querySelector(".rate");
 let ratings = document.querySelectorAll(".rate");
 let feedback_page = document.querySelector(".rate-card_components");
 let thanks_page = document.querySelector(".thanks-card_components");
-let user_rating = 3
-let user_submission = document.querySelector('#user-choice')
-
-
-
+let user_submission = document.querySelector("#user-choice");
 
 ratings.forEach((btn) => btn.addEventListener("click", handleRating));
 
 submit_btn.addEventListener("click", submit);
 
+function submit() {
+  if (user_rating) {
+    user_submission.innerHTML = user_rating;
+    feedback_page.classList.add("hide");
+    thanks_page.classList.remove("hide");
+  }
 
-function submit(){
-user_submission.innerHTML = user_rating
-feedback_page.classList.add('hide');
-thanks_page.classList.remove('hide')
 }
 
+function handleRating(e) {
+  ratings.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-function handleRating(e){
-ratings.forEach(btn  => {
-    btn.classList.remove('active')
-});
-
-if (e.target.classList.contains('rate')){
-    e.target.classList.add('active')
-}
-user_rating = e.target.id
+  if (e.target.classList.contains("rate")) {
+    e.target.classList.add("active");
+  }
+  submit_btn.classList.remove('disabled')
+  user_rating = e.target.id;
 }
